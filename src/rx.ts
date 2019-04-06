3508ccde5b2af4c08953e2ab64663369a5cc91a3
sisisin <azsisisin@gmail.com>
2019-04-06 11:39:03 +0900

    fix: type error

--- src/rx.ts
+++ src/rx.ts
@@ -1,12 +1,12 @@
-import { Observable } from 'rxjs';
+import { Observable, ObservableInput } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 export * from 'typeless/rx';
 
-export const catchLog = <T, R>(
-  fn: (err: Error, source: Observable<T>) => Observable<R>
+export const catchLog = <T, O extends ObservableInput<any>>(
+  fn: (err: any, caught: Observable<T>) => O
 ) =>
-  catchError<T, R>((err, source) => {
+  catchError<T, O>((err, source) => {
     if (process.env.NODE_ENV !== 'test') {
       // tslint:disable-next-line:no-console
       console.error(err);
