8e6d60273bb50a53438d74c0c6e659a9ee0bbdc7
sisisin <azsisisin@gmail.com>
2019-04-13 18:20:33 +0900

    feat: borrow and return by barcode

--- src/features/barcodeLoad/module.tsx
+++ src/features/barcodeLoad/module.tsx
@@ -1,42 +1,86 @@
 import React from 'react';
 import { cameraRepository } from 'src/services/CameraRepository';
+import { bookRepository } from 'src/services/ServiceContainer';
 import { createEpic, createReducer, useModule } from 'typeless';
 import * as Rx from 'typeless/rx';
+import { BookActions } from '../book/interface';
+import { userIdQuery } from '../global/query';
 import { BarcodeLoadView } from './components/BarcodeLoadView';
 import { BarcodeLoadActions, BarcodeLoadState, MODULE } from './interface';
 
 // --- Epic ---
-export const epic = createEpic(MODULE).on(BarcodeLoadActions.enableCamera, () =>
-  Rx.of().pipe(
-    Rx.tap(() => {
-      cameraRepository.grantCameraPermission();
-    }),
-    Rx.ignoreElements()
+export const epic = createEpic(MODULE)
+  .on(BarcodeLoadActions.enableCamera, () =>
+    Rx.of().pipe(
+      Rx.tap(() => {
+        cameraRepository.grantCameraPermission();
+      }),
+      Rx.ignoreElements()
+    )
   )
-);
+  .on(BarcodeLoadActions.detectBarcode, ({ data }) => {
+    const isbnCodePrefix = '978';
+    if (data.codeResult.code.indexOf(isbnCodePrefix) === -1) {
+      return Rx.empty();
+    } else {
+      return Rx.of(
+        BarcodeLoadActions.fetchBookFromBarcode(data.codeResult.code)
+      );
+    }
+  })
+  .on(BarcodeLoadActions.fetchBookFromBarcode, ({ code }, { getState }) => {
+    return Rx.fromPromise(bookRepository.findBooksByIsbn(code)).pipe(
+      Rx.map(books => {
+        const userId = userIdQuery(getState().global);
+        const borrowedByMe = books.find(b => b.borrowedBy === userId);
+        const stock = books.find(b => !b.borrowedBy);
+        const target = !!borrowedByMe ? borrowedByMe : stock;
+        return BarcodeLoadActions.fetchBookFromBarcodeFullfilled(target);
+      })
+    );
+  })
+  .on(BookActions.borrowBookByIdFulfilled, ({ book }) => {
+    alert(`${book.title}を借りました`);
+    return Rx.empty();
+  })
+  .on(BookActions.returnBookByIdFulfilled, ({ book }) => {
+    alert(`${book.title}を返却しました`);
+    return Rx.empty();
+  });
 
 // --- Reducer ---
 const initialState: BarcodeLoadState = {
   isCameraSupported: false,
   isCameraEnabled: cameraRepository.isCameraPermissionGranted(),
   targetBook: undefined,
+  isProcessingBook: false,
 };
 
 export const reducer = createReducer(initialState)
   .on(BarcodeLoadActions.$mounted, state => {
     state.isCameraSupported =
       navigator.mediaDevices && !!navigator.mediaDevices.getUserMedia;
+    state.targetBook = undefined;
+    state.isProcessingBook = false;
   })
   .on(BarcodeLoadActions.enableCamera, state => {
     state.isCameraEnabled = true;
   })
-  .on(BarcodeLoadActions.detectBarcode, (state, { data }) => {
+  .on(BarcodeLoadActions.fetchBookFromBarcode, state => {
     state.isCameraEnabled = false;
-    state.targetBook = {
-      id: 'hoge',
-      title: 'kari',
-      isbn: data.codeResult.code,
-    };
+  })
+  .onMany([BookActions.borrowBookById, BookActions.returnBookById], state => {
+    state.isProcessingBook = true;
+  })
+  .onMany(
+    [BookActions.borrowBookByIdFulfilled, BookActions.returnBookByIdFulfilled],
+    state => {
+      state.isProcessingBook = false;
+      state.targetBook = undefined;
+    }
+  )
+  .on(BarcodeLoadActions.fetchBookFromBarcodeFullfilled, (state, { book }) => {
+    state.targetBook = book;
   });
 
 // --- Module ---
--- src/features/book/bookList/components/BookBorrowAndReturnBottun.tsx
+++ src/features/book/bookList/components/BookBorrowAndReturnBottun.tsx
@@ -6,6 +6,7 @@ interface Props {
   onReturn: (bookId: string) => void;
   book: Book;
   userId: string;
+  disabled?: boolean;
 }
 
 export const BookBorrowAndReturnButton: React.SFC<Props> = ({
@@ -13,13 +14,22 @@ export const BookBorrowAndReturnButton: React.SFC<Props> = ({
   onReturn,
   book,
   userId,
+  disabled,
 }) => {
   if (book.borrowedBy === userId) {
     // 借りている時
-    return <button onClick={() => onReturn(book.id)}>返却</button>;
+    return (
+      <button onClick={() => onReturn(book.id)} disabled={disabled}>
+        返却
+      </button>
+    );
   } else if (!book.borrowedBy) {
     // 借りられていない時
-    return <button onClick={() => onBorrow(book.id)}>貸出</button>;
+    return (
+      <button onClick={() => onBorrow(book.id)} disabled={disabled}>
+        貸出
+      </button>
+    );
   } else {
     // 借りられている時
     return <button disabled>{book.borrowedBy}が借りています</button>;
