59af3ee540af612295ebd885c838dadf0bf027c8
sisisin <azsisisin@gmail.com>
2019-04-14 16:02:13 +0900

    feat: setup grommet

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,4 @@
+import { Grommet } from 'grommet';
 import React, { useEffect } from 'react';
 import * as R from 'remeda';
 import useBookModule from 'src/features/book/module';
@@ -5,29 +6,8 @@ import { GlobalActions } from 'src/features/global/interface';
 import { useGlobalModule } from 'src/features/global/module';
 import { useRouterModule } from 'src/features/router/module';
 import { authService } from 'src/services/ServiceContainer';
-import { createGlobalStyle } from 'styled-components';
 import { useActions, useMappedState } from 'typeless';
 import { RouteResolver } from './RouteResolver';
-
-const GlobalStyle = createGlobalStyle`
-  *, ::after, ::before {
-      box-sizing: border-box;
-  }
-
-  html, body, #app {
-    height: 100%;
-  }
-
-  body {
-    background-color: #f5f5f5;
-    margin: 0;
-    font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";
-    font-size: 16px;
-    font-weight: 400;
-    line-height: 1.5;
-    color: #212529;
-  }
-`;
 
 export const App = () => {
   useRouterModule();
@@ -38,10 +18,5 @@ export const App = () => {
   const { isLoaded } = useMappedState(state =>
     R.pick(state.global, ['isLoaded'])
   );
-  return (
-    <>
-      {isLoaded && <RouteResolver />}
-      <GlobalStyle />
-    </>
-  );
+  return <Grommet plain>{isLoaded && <RouteResolver />}</Grommet>;
 };
