dacfb999272d87518729c386852ae50e1da460b4
watiko <service@mail.watiko.net>
2019-04-21 18:14:04 +0900

    feat: CloudFunctionsの設定・コード

---
+++ README.md
@@ -0,0 +1,5 @@
+# bibliotheca-pwa
+
+## Requirements
+
+- Node.js 8.x (Cloud Functions for Firebase のため)
---
+++ functions/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    '@typescript-eslint/adjacent-overload-signatures': 'error',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/prefer-interface': 'off',
+    '@typescript-eslint/no-parameter-properties': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
+};
