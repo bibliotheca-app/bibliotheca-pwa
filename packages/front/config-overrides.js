ccb04cb76ee3c6e9cc53cf8f36b48e6912473b61
sisisin <azsisisin@gmail.com>
2019-05-08 04:48:55 +0900

    fix: apply disable config when build
    ref. https://github.com/facebook/create-react-app/issues/3886#issuecomment-435086178

--- packages/front/config-overrides.js
+++ packages/front/config-overrides.js
@@ -1,6 +1,19 @@
-module.exports = function override(config) {
+module.exports = function override(config, env) {
   config.plugins = config.plugins.filter(plugin => {
     return plugin.constructor.name !== 'ForkTsCheckerWebpackPlugin';
+  });
+  config.module.rules.some(rule => {
+    if (Array.isArray(rule.use)) {
+      const eslintUse = rule.use.find(item =>
+        Object.keys(item.options).find(key => key === 'useEslintrc'),
+      );
+      const eslintOptions = eslintUse && eslintUse.options;
+      if (eslintOptions) {
+        eslintOptions.useEslintrc = true;
+        return true;
+      }
+    }
+    return false;
   });
 
   return config;
