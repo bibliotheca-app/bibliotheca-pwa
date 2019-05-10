e6e7d1ff77c5f373108a4d47b412566f1f5e1466
watiko <service@mail.watiko.net>
2019-05-11 00:55:29 +0900

    feat(book-detail): progress bar

--- packages/front/src/bibliotheca/features/book/detail/components/BookDetailView.tsx
+++ packages/front/src/bibliotheca/features/book/detail/components/BookDetailView.tsx
@@ -1,12 +1,26 @@
 import { BookDetail } from 'bibliotheca/components/BookDetail';
+import { Delay } from 'bibliotheca/components/Delay';
+import { NotFoundError } from 'navi';
 import React from 'react';
+import { Instagram } from 'react-content-loader';
+import { useCurrentRoute } from 'react-navi';
 import { useMappedState } from 'typeless';
 
 export const BookDetailView = () => {
   const book = useMappedState(state => state.bookDetail.selectedBook);
+  const error = useMappedState(state => state.bookDetail.findBookError);
+  const currentRoute = useCurrentRoute();
+
+  if (error) {
+    throw new NotFoundError(currentRoute.url.pathname);
+  }
 
   if (!book) {
-    return <>loading</>;
+    return (
+      <Delay wait={500}>
+        <Instagram />
+      </Delay>
+    );
   }
 
   return <BookDetail book={book} />;
