8f346f0b3cfc97d63ec2d180aa69bc2e3b754411
sisisin <azsisisin@gmail.com>
2019-05-19 20:14:06 +0900

    fix: initializeApp must call once

--- packages/functions/src/bookList/index.ts
+++ packages/functions/src/bookList/index.ts
@@ -1,4 +1,3 @@
-import * as admin from 'firebase-admin';
 import { DocumentSnapshot } from '@google-cloud/firestore';
 import { Change, EventContext } from 'firebase-functions';
 
@@ -37,53 +36,53 @@ export const writeAllBook = async (db: FirebaseFirestore.Firestore) => {
   return writeBookEntries(db, bookEntries);
 };
 
-export async function onBookWrite(
-  change: Change<DocumentSnapshot>,
-  context: EventContext,
-): Promise<void> {
-  admin.initializeApp();
-  const db = admin.firestore();
-  const bookId: string = context.params.bookId;
+export function initOnBookWrite(db: FirebaseFirestore.Firestore) {
+  return async function onBookWrite(
+    change: Change<DocumentSnapshot>,
+    context: EventContext,
+  ): Promise<void> {
+    const bookId: string = context.params.bookId;
 
-  // console.log(`change: ${JSON.stringify(change)}`);
-  // console.log(`context: ${JSON.stringify(context)}`);
+    // console.log(`change: ${JSON.stringify(change)}`);
+    // console.log(`context: ${JSON.stringify(context)}`);
 
-  // https://firebase.google.com/docs/firestore/quotas
-  // max document size: 1,048,576 bytes
+    // https://firebase.google.com/docs/firestore/quotas
+    // max document size: 1,048,576 bytes
 
-  const cachedBookEntries = await fetchCachedBookEntriesMapAsync(db);
+    const cachedBookEntries = await fetchCachedBookEntriesMapAsync(db);
 
-  if (!change.before.exists) {
-    console.log(`作成: ${bookId}`);
-    await writeBookEntries(db, [
-      ...cachedBookEntries,
-      { id: bookId, data: change.after.data() as any },
-    ]);
-  } else if (!change.after.exists) {
-    console.log(`削除: ${bookId}`);
-    await writeBookEntries(
-      db,
-      cachedBookEntries.filter(entry => {
-        return entry.id !== bookId;
-      }),
-    );
-  } else {
-    console.log(`更新/上書き: ${bookId}`);
-    await writeBookEntries(
-      db,
-      cachedBookEntries.map(entry => {
-        if (entry.id !== bookId) {
-          return entry;
-        }
-        return { id: bookId, data: change.after.data() as any };
-      }),
-    );
-  }
+    if (!change.before.exists) {
+      console.log(`作成: ${bookId}`);
+      await writeBookEntries(db, [
+        ...cachedBookEntries,
+        { id: bookId, data: change.after.data() as any },
+      ]);
+    } else if (!change.after.exists) {
+      console.log(`削除: ${bookId}`);
+      await writeBookEntries(
+        db,
+        cachedBookEntries.filter(entry => {
+          return entry.id !== bookId;
+        }),
+      );
+    } else {
+      console.log(`更新/上書き: ${bookId}`);
+      await writeBookEntries(
+        db,
+        cachedBookEntries.map(entry => {
+          if (entry.id !== bookId) {
+            return entry;
+          }
+          return { id: bookId, data: change.after.data() as any };
+        }),
+      );
+    }
+  };
 }
 
-export async function onBookListUpdateTime(_: EventContext): Promise<void> {
-  admin.initializeApp();
-  const db = admin.firestore();
-  console.log('bookLists更新');
-  await writeAllBook(db);
+export function initOnBookListUpdateTime(db: FirebaseFirestore.Firestore) {
+  return async function onBookListUpdateTime(_: EventContext): Promise<void> {
+    console.log('bookLists更新');
+    await writeAllBook(db);
+  };
 }
