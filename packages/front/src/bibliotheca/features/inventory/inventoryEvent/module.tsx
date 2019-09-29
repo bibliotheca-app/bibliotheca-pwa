638d5bb85f36d2c6049b4cea1352b0c3be8645d5
sisisin <azsisisin@gmail.com>
2019-09-29 16:42:39 +0900

    refactor: promisify

--- packages/front/src/bibliotheca/features/inventory/inventoryEvent/module.tsx
+++ packages/front/src/bibliotheca/features/inventory/inventoryEvent/module.tsx
@@ -7,16 +7,15 @@ import {
 } from 'bibliotheca/services/ServiceContainer';
 import { InventoryEventDoing } from 'bibliotheca/types';
 import React from 'react';
-import * as Rx from 'typeless/rx';
+import { getInventoryBookModuleState } from '../inventoryBookModule/interface';
 import { useInventoryBookModule } from '../inventoryBookModule/module';
 import { InventoryEventView } from './components/InventoryEventView';
 import { handle, InventoryEventActions, InventoryEventState } from './interface';
-import { getInventoryBookModuleState } from '../inventoryBookModule/interface';
 
 // --- Epic ---
 export const epic = handle
   .epic()
-  .on(InventoryEventActions.toMissingAll, () => {
+  .on(InventoryEventActions.toMissingAll, async () => {
     const { booksInList, event } = getInventoryBookModuleState();
 
     const uncheckedBooks = findUncheckedOnlyList(
@@ -24,23 +23,18 @@ export const epic = handle
       booksInList,
     );
 
-    if (uncheckedBooks.length === 0) return Rx.empty();
+    if (uncheckedBooks.length === 0) return null;
 
     const msg =
       '以下の書籍を紛失状態にします。よろしいですか？' +
       uncheckedBooks.map(b => b.title).join('\n');
     if (window.confirm(msg)) {
-      return Rx.from(
-        inventoryEventRepository.addInventoryBook(
-          uncheckedBooks.map(b => ({ status: 'missing' as const, bookId: b.id })),
-        ),
-      ).pipe(
-        Rx.map(() => {
-          return NotificationActions.notifyMessage('全て紛失ステータスに変えました');
-        }),
+      await inventoryEventRepository.addInventoryBook(
+        uncheckedBooks.map(b => ({ status: 'missing' as const, bookId: b.id })),
       );
+      return NotificationActions.notifyMessage('全て紛失ステータスに変えました');
     } else {
-      return Rx.empty();
+      return null;
     }
   })
   .on(InventoryEventActions.toCheckStatus, async ({ book }) => {
@@ -58,7 +52,7 @@ export const epic = handle
       return null;
     }
   })
-  .on(InventoryEventActions.submitInventory, () => {
+  .on(InventoryEventActions.submitInventory, async () => {
     const msg =
       '棚卸しを完了します。\n＊＊全ての紛失ステータスの書籍が削除されます＊＊\nよろしいですか？';
     if (window.confirm(msg)) {
@@ -74,15 +68,12 @@ export const epic = handle
         .map(b => ({ ...b, deletedAt: new Date() }));
 
       // todo: validate submit if unchecked book exists
-      return Rx.from(
-        (async () => {
-          await inventoryLogRepository.add({ date, status: 'done', books: inventoriedBooks });
-          await inventoryEventRepository.close();
-          await bookRepository.bulkUpdate(missingBooks);
-        })(),
-      ).pipe(Rx.map(() => NotificationActions.notifyMessage('棚卸しを完了しました')));
+      await inventoryLogRepository.add({ date, status: 'done', books: inventoriedBooks });
+      await inventoryEventRepository.close();
+      await bookRepository.bulkUpdate(missingBooks);
+      return NotificationActions.notifyMessage('棚卸しを完了しました');
     } else {
-      return Rx.empty();
+      return null;
     }
   });
 // --- Reducer ---
