275c3562f0d0b03c9a1d65c08b7eadb63db52026
sisisin <azsisisin@gmail.com>
2019-11-27 02:23:26 +0900

    chore: prettierのconfigを分離

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
